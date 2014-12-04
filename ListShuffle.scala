7946a21ba9b149d97f42c106ef9d9789afe6af3d
Antonio Prestes García <antonio.garcia2@gmail.com>
2014-12-04 16:52:48 +0100

    Create ListShuffle.scala

---
+++ ListShuffle.scala
@@ -0,0 +1,10 @@
+// -- | Shuffle' auxiliary
+def shufflep(r: Int,l1: List,l2: List) {
+}
+
+// -- | The main entry point.
+def main(args: Array[String]) {
+	val v: List[Int] = List.range(1,101)
+	println("List shuffle!")
+	println(v)
+}
